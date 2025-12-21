98e6ddf0af1ce1e5e9e37b2a48d30547404e03bf
TestUser <test@example.com>
2025-12-21 04:58:46 -0500

    BUG: ... in collection of *mmwide.csv

--- antsxmm/_version.py
+++ antsxmm/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '1.0.1.dev12'
-__version_tuple__ = version_tuple = (1, 0, 1, 'dev12')
+__version__ = version = '1.0.1.dev13'
+__version_tuple__ = version_tuple = (1, 0, 1, 'dev13')
 
-__commit_id__ = commit_id = 'geae2123c1'
+__commit_id__ = commit_id = 'g91ee25164'
