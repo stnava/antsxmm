438a6532d51d6369bcf1c991925069c362d31735
stnava <stnava@gmail.com>
2026-03-05 09:15:12 -0500

    PERF: test style

--- antsxmm/_version.py
+++ antsxmm/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '1.0.1.dev31'
-__version_tuple__ = version_tuple = (1, 0, 1, 'dev31')
+__version__ = version = '1.0.1.dev32'
+__version_tuple__ = version_tuple = (1, 0, 1, 'dev32')
 
-__commit_id__ = commit_id = 'g474657ab3'
+__commit_id__ = commit_id = 'g7425942d0'
